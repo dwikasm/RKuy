00a2353e6af036d81af8c3de3c068277b88feec6
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-19 13:53:03 +0700

    delivery order

--- resources/views/transaksi/deliveryorder.blade.php
+++ resources/views/transaksi/deliveryorder.blade.php
@@ -1,6 +1,118 @@
 @extends('adminlte::page')
-@section('title', 'AdminLTE')
-@section('content_header')<h1>Dashboard</h1>@stop
+@section('title', 'SI PT Maju Jaya')
+@section('content_header')<h1>Buat Delivery Order</h1>@stop
 @section('content')
-<h4>belum ada diisi</h4>
+<body onload="window.print();">
+  <div class="row">
+    <div class="col-md-12">      
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-md-12">
+              <h2>Delivery Order</h2>
+            </div>
+          </div>
+          <br>
+          <!-- info row -->
+          <div class="row invoice-info">
+            <div class="col-sm-4 invoice-col">
+              Dari
+              <address>
+                <strong>PT Maju Jaya</strong><br>
+                Jalan Sasak 2 No.14 Kebon Jeruk<br>
+                Jakarta Barat, 11550<br>
+                Phone: (804) 123-5432<br>
+                Email: majujaya@maju.com
+              </address>
+            </div>
+            <!-- /.col -->
+            <div class="col-sm-4 invoice-col">
+              Kepada
+              <address>
+                <strong>Perusahaan Westra</strong><br>
+                Jalan Bumi 3 No.19 Kemanggisan<br>
+                Surabaya, 61120<br>
+                Phone: (555) 539-1037<br>
+                Email: joko@westra.com
+              </address>
+            </div>
+            <!-- /.col -->
+            <div class="col-sm-4 invoice-col">
+              <b>Delivery Order #007612</b><br>
+              <br>
+              <b>Order ID:</b> 4F3S8J<br>
+              <b>Tanggal:</b> 2/22/2014<br>
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+
+          <!-- Table row -->
+          <div class="row">
+            <div class="col-xs-12 table-responsive">
+              <table class="table table-striped">
+                <thead>
+                <tr>
+                  <th>Jumlah</th>
+                  <th>Produk</th>
+                  <th>Deskripsi</th>
+                  <th>Subtotal</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Oxygen Checker</td>
+                  <td>Alat untuk memeriksa kadar oksigen di udara</td>
+                  <td>Rp. 1000.000,00</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>CO2 Checker</td>
+                  <td>Alat untuk memeriksa kadar co2 di udara</td>
+                  <td>Rp. 900.000,00</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+
+          <div class="row">
+            <!-- accepted payments column -->
+            <div class="col-xs-6">
+              
+            </div>
+            <!-- /.col -->
+            <div class="col-xs-6">
+              <p class="lead">Tanggal 22 April 2017</p>
+
+              <div class="table-responsive">
+                <table class="table">
+                  <tr>
+                    <th style="width:50%">Subtotal:</th>
+                    <td>Rp. 1.900.000,00</td>
+                  </tr>
+                  <tr>
+                    <th>Tax (5%)</th>
+                    <td>Rp. 95.000,00</td>
+                  </tr>
+                  <tr>
+                    <th>Total:</th>
+                    <td>Rp. 1.995.000,00</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+
+        </div>
+        <!-- /. panel body -->
+      </div>
+    </div>
+  
+  </div>
 @stop
