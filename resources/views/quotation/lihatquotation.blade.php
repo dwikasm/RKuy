ab8312074c3841837e5e4d012d6819f455adc5c5
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-23 00:04:54 +0700

    lihat quotation view

--- resources/views/quotation/lihatquotation.blade.php
+++ resources/views/quotation/lihatquotation.blade.php
@@ -4,12 +4,12 @@
 @section('content')
     <div class="panel panel-default">
       <div class="panel-heading">
-        <h4><i class="fa fa-home"></i> Tabel Quotation</h4>
+        <h4><i class="fa fa-list"></i> Tabel Quotation</h4>
       </div>
       <div class="panel-body">
         <div class=row>
           <div class="col-md-12" style="padding-bottom: 1.5rem">
-            <a href="{{Url('quotation/tambah')}}" class="btn btn-primary"><i class="fa fa-plus-circle"></i> Tambah Quotation</a>
+            <a href="{{Url('quotation/tambah')}}" class="btn btn-primary btn-sm"><i class="fa fa-plus-circle"></i> Tambah Quotation</a>
           </div>
           <!-- /.col-md-12 -->
           <div class="col-md-12">                                                                                      
@@ -17,21 +17,19 @@
               <table id="myTable" class="table table-bordered table-condensed table-hover">
                 <thead>
                   <tr>
-                    <th>ID Quotation</th>
+                    <th width="80">ID Quotation</th>
                     <th>Waktu Quotation</th>
-                    <th>Customer</th>
                     <th>Total Harga</th>
-                    <th>Detail</th>
+                    <th width="80">Opsi</th>
                   </tr>
                 </thead>
                 <tbody>
                   @foreach($quotations as $key => $value)
               <tr>
-                <td>{{ $value->id_quo }}</td>
+                <td class="text-center">{{ $value->id_quo }}</td>
                 <td>{{ $value->created_at }}</td>
-                <td>{{ $value->total }}</td>
-                <td></td>
-                <td><a href="{{Url('/quotation/cetak')}}"><button type="button" class="btn btn-default">Lihat</button></a>
+                <td>Total Harganya </td>
+                <td><a href="{{Url('/transaksi/detail/'.$value->id_quo)}}"<button type="button" class="btn btn-default btn-sm"/><i class="fa fa-eye"></i> Lihat</button></a></td>
               </tr>
               @endforeach
                 </tbody>
