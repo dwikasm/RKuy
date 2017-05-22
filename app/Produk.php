d65df7d8ecf2fd0c357b564beedba0b2cef7dba9
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-22 21:17:50 +0700

    benerin model produk yang konflik

--- app/Produk.php
+++ app/Produk.php
@@ -7,13 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Produk extends Model
 {
     protected $table = 'produk';
-<<<<<<< HEAD
-
-    protected $fillable = [
-        'id_pro', 'harga', 'stok',
-=======
     protected $fillable = [
         'id_pro', 'nama_pro', 'harga', 'stok',
->>>>>>> 750096a4d6be91a26c8626632f823fccd2451038
     ];
 }
