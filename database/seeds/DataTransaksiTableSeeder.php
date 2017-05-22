06b04840d1bd724ad338703ef76511d4a665207d
Syukron Rifail M <syukronrifai@gmail.com>
2017-05-22 19:39:49 +0700

    Add one row

--- database/seeds/DataTransaksiTableSeeder.php
+++ database/seeds/DataTransaksiTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Transaksi;
 
 class DataTransaksiTableSeeder extends Seeder
 {
@@ -13,5 +14,11 @@ class DataTransaksiTableSeeder extends Seeder
     public function run()
     {
         DB::table('data_transaksi')->delete();
+
+        $data = new Transaksi();
+        $data->id_quo = 1;
+        $data->customer = "Kukuh";
+        $data->alamat = "Jalan Gebang Kidul 40B";
+        $data->save();
     }
 }
