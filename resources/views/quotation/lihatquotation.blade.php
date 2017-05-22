4dd94732d58075d3fda5ad5afe8c91e8a737991c
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-22 21:13:18 +0700

    rapihin doang

--- resources/views/quotation/lihatquotation.blade.php
+++ resources/views/quotation/lihatquotation.blade.php
@@ -8,26 +8,17 @@
 @section('title', 'AdminLTE')
 @section('content_header')<h1>Dashboard</h1>@stop
 @section('content')
-<div class="panel panel-default"><div class="panel-body">
-<h4><i class="fa fa-home"></i> Tabel Quotation</h4><hr>
-<div class=row><div class="col-md-6">
-     <a href="{{Url('quotation/tambah')}}" class="btn btn-primary">
-       <i class="fa fa-plus-circle"></i> Tambah Quotation</a></div>
-  <div class="col-md-2"> </div>
-<!-- Form Pencarian -->
-  <!-- <div class="col-md-4">
-    {!! Form::open(['method'=>'GET','url'=>'caridosen','role'=>'search'])  !!}
-      <div class="input-group custom-search-form">
-          <input type="text" class="form-control" name="search" placeholder="Search...">
-          <span class="input-group-btn">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="submit"><i class="fa fa-search"></i> Cari</button>
-            </span>
-          </span>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h4><i class="fa fa-home"></i> Tabel Quotation</h4>
+    <div class=row>
+      <div class="col-md-6">
+        <a href="{{Url('quotation/tambah')}}" class="btn btn-primary"><i class="fa fa-plus-circle"></i> Tambah Quotation</a>
       </div>
-      {!! Form::close() !!}
-  </div> -->
-  <div class="container">
+      <div class="col-md-2"> 
+
+      </div>
+      <div class="container">
       <br><br>                                                                                        
       <div class="table-responsive">          
         <table class="table">
@@ -41,26 +32,21 @@
               </tr>
           </thead>
           <tbody>
-          @foreach($quotations as $key => $value)
+            @foreach($quotations as $key => $value)
             <tr>
               <td></td>
               <td>{{ $value->id_quo }}</td>
               <td>{{ $value->total }}</td>
               <td>{{ $value->created_at }}</td>
-              <td>
-              <a href="{{Url('/quotation/cetak')}}">
-                <button type="button" class="btn btn-default">Lihat</button></a>
-                <!-- <button type="button" class="btn btn-primary">Edit</button> -->
-                <button type="button" class="btn btn-danger">Hapus</button>
-              </td>
+              <td><a href="{{Url('/quotation/cetak')}}"><button type="button" class="btn btn-default">Lihat</button></a><!-- <button type="button" class="btn btn-primary">Edit</button> --><button type="button" class="btn btn-danger">Hapus</button></td>
             </tr>
             @endforeach
           </tbody>
-       </table>
+        </table>
       </div>
+    </div>
   </div>
-</div>
-<br>
+  <br>
 </div>
 <br>
 @stop
