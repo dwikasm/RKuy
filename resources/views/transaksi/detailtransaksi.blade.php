dac5f14246db338a2f875c141fa630baabc07cca
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-22 22:02:09 +0700

    lihat dan detail transaksi

--- resources/views/transaksi/detailtransaksi.blade.php
+++ resources/views/transaksi/detailtransaksi.blade.php
@@ -1,80 +1,27 @@
 @extends('adminlte::page')
-@section('title', 'AdminLTE')
-@section('adminite_css')
+@section('title', 'SI Maju Jaya')
 @section('content_header')@stop
 @section('content')
-
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css
-">
-<div class=row>
-    
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/deliveryorder')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Delivery Order</a>
-    </div>
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/paymentreceipt')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Payment Receipt</a>
-    </div>
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/proofoir')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Proof of Item Receipt</a>
-    </div>
-</div>
-
-
-
- <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Tabel Transaksi</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="myTable" class="table table-bordered table-hover">
-                <thead>
-                  <tr>
-                    <th>Nama Barang</th>
-                    <th>Jumlah</th>
-                    <th>Harga Satuan</th>
-                    <th>Total Harga</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Kursi Roda</td>
-                    <td>5</td>
-                    <td>Rp6.000.000</td>
-                    <td>Rp.30.000.000</td>
-                  </tr>
-                  <tr>
-                    <td>Stetoscope</td>
-                    <td>50</td>
-                    <td>Rp1.000.000</td>
-                    <td>Rp.50.000.000</td>
-                  </tr>
-                  <tr>
-                    <td></td>
-                    <td></td>
-                    <td>Total :</td>
-                    <td>Rp.80.000.000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-<script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js "></script>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4><i class="fa fa-eye"></i> Detail Transaksi</h4>
+      </div>
+      <div class="panel-body">
+        <div class="row">
           
-<script>
-  $(document).ready(function(){
-    $('#myTable').DataTable();
-});
-</script>
-
+        </div>
+        <!-- /.row -->
+      </div>
+    </div>  
 @stop
-
+@section('css')
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
+@stop
+@section('js')
+    <script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js "></script>
+    <script>
+      $(document).ready(function(){
+        $('#myTable').DataTable();
+    });
+    </script>
+@stop
--- resources/views/transaksi/lihattransaksi.blade.php
+++ resources/views/transaksi/lihattransaksi.blade.php
@@ -1,125 +1,58 @@
 @extends('adminlte::page')
-<<<<<<< HEAD
-@section('title', 'AdminLTE')
-@section('adminite_css')
+@section('title', 'SI Maju Jaya')
 @section('content_header')@stop
 @section('content')
-
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css
-">
-<div class="panel panel-default"><div class="panel-body">
-<h4><i class="fa fa-home"></i> Tabel Transaksi</h4><hr>
-<div class=row>
-  <div class="col-md-2">
-     <a href="{{Url('/transaksi/tambah')}}" class="btn btn-primary">
-       <i class="fa fa-plus-circle"></i> Tambah Transaksi</a>
-    </div>
-</div>
-
- <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Tabel Transaksi</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="lihat_transaksi" class="table table-bordered table-hover">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4><i class="fa fa-home"></i> Tabel Transaksi</h4>
+      </div>
+      <div class="panel-body">
+        <div class=row>
+          <div class="col-md-12" style="padding-bottom: 1.5rem">
+            <a href="{{Url('/transaksi/tambah')}}" class="btn btn-primary btn-sm"><i class="fa fa-plus-circle"></i> Tambah Transaksi</a>
+          </div>
+          <!-- /.col-md-12 -->
+          <div class="col-md-12">                                                                                      
+            <div class="table-responsive">          
+              <table id="myTable" class="table table-bordered table-condensed table-hover">
                 <thead>
                   <tr>
-                    <th>Id Transaksi</th>
+                    <th>Tanggal Dibuat</th>
+                    <th>ID Transaksi</th>
+                    <th>ID Quotation</th>
                     <th>Customer</th>
                     <th>Alamat</th>
-                    <th>Waktu Transaksi</th>
-                    <th>Action</th>
+                    <th>Detail</th>
                   </tr>
                 </thead>
                 <tbody>
-                  @foreach($transaksis as $key => $value)
+                @foreach($transaksis as $key => $value)
                   <tr>
-                    
+                    <td>{{ $value->created_at }}</td>
                     <td>{{ $value->id_tr }}</td>
+                    <td>{{ $value->id_quo }}</td>
                     <td>{{ $value->customer }}</td>
                     <td>{{ $value->alamat }}</td>
-                    <td>{{ $value->created_at }}</td>
-                    <td>
-                    <a href="{{Url('/transaksi/detail')}}"
-                      <button type="button" class="btn btn-default">Lihat</button></a>
-                      <button type="button" class="btn btn-danger">Hapus</button>
-                    </td>
+                    <td><a href="{{Url('/transaksi/detail')}}"<button type="button" class="btn btn-default btn-sm"/><i class="fa fa-eye"></i> Lihat</button></a></td>
                   </tr>
                   @endforeach
                 </tbody>
               </table>
             </div>
-            <!-- /.box-body -->
           </div>
-          <!-- /.box -->
-
-=======
-@section('title', 'SI Maju Jaya')
-@section('content_header')@stop
-@section('content')
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css
-">
-<div class="panel panel-default">
-  <div class="panel-body">
-    <h4><i class="fa fa-home"></i> Tabel Transaksi</h4>
-    <div class=row>
-      <div class="col-md-12 text-left" style="margin-bottom: 1.5rem">
-        <a href="{{Url('/transaksi/tambah')}}" class="btn btn-primary btn-sm"><i class="fa fa-plus-circle"></i> Tambah Transaksi</a>
-      </div>
-      <!-- /.col-md-12 -->
-      <div class="col-md-12">                                                                                      
-        <div class="table-responsive">          
-          <table id="myTable" class="table table-bordered table-condensed table-hover">
-            <thead>
-              <tr>
-                <th>Tanggal Dibuat</th>
-                <th>ID Transaksi</th>
-                <th>ID Quotation</th>
-                <th>Customer</th>
-                <th>Alamat</th>
-                <th>Detail</th>
-              </tr>
-            </thead>
-            <tbody>
-            @foreach($transaksis as $key => $value)
-              <tr>
-                <td>{{ $value->created_at }}</td>
-                <td>{{ $value->id_tr }}</td>
-                <td>{{ $value->id_quo }}</td>
-                <td>{{ $value->customer }}</td>
-                <td>{{ $value->alamat }}</td>
-                <td><a href="{{Url('/transaksi/detail')}}"<button type="button" class="btn btn-default btn-sm"/><i class="fa fa-eye"></i> Lihat</button></a></td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
         </div>
+        <!-- /.row -->
       </div>
-    </div>
-    <!-- /.row -->
-  </div>
-</div>
-
->>>>>>> cdd38d306d3b5c3be2ee81553b685e264d3ac73b
-<script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js "></script>
-          
-<script>
-  $(document).ready(function(){
-<<<<<<< HEAD
-    $('#lihat_transaksi').DataTable();
-});
-</script>
-
+    </div>  
 @stop
-
-=======
-    $('#myTable').DataTable();
-});
-</script>
+@section('css')
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
 @stop
->>>>>>> cdd38d306d3b5c3be2ee81553b685e264d3ac73b
+@section('js')
+    <script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js "></script>
+    <script>
+      $(document).ready(function(){
+        $('#myTable').DataTable();
+    });
+    </script>
+@stop
