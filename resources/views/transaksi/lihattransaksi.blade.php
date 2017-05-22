71a8e78e372445a55646534a737c1da7c4cd6ceb
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-22 21:42:37 +0700

    view lihat transaksi

--- resources/views/transaksi/lihattransaksi.blade.php
+++ resources/views/transaksi/lihattransaksi.blade.php
@@ -1,74 +1,55 @@
 @extends('adminlte::page')
-@section('title', 'AdminLTE')
-@section('content_header')<h1>Dashboard</h1>@stop
+@section('title', 'SI Maju Jaya')
+@section('content_header')@stop
 @section('content')
-<div class="panel panel-default"><div class="panel-body">
-<h4><i class="fa fa-home"></i> Tabel Transaksi</h4><hr>
-<div class=row>
-  <div class="col-md-2">
-     <a href="{{Url('/transaksi/tambah')}}" class="btn btn-primary">
-       <i class="fa fa-plus-circle"></i> Tambah Transaksi</a>
-    </div>
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/deliveryorder')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Delivery Order</a>
-    </div>
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/paymentreceipt')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Payment Receipt</a>
-    </div>
-    <div class="col-md-2">
-      <a href="{{Url('/transaksi/proofoir')}}" class="btn btn-default">
-       <i class="fa fa-plus-circle"></i> Buat Proof of Item Receipt</a>
-    </div>
-</div>
-<!-- Form Pencarian -->
-  <!-- <div class="col-md-4">
-    {!! Form::open(['method'=>'GET','url'=>'caridosen','role'=>'search'])  !!}
-      <div class="input-group custom-search-form">
-          <input type="text" class="form-control" name="search" placeholder="Search...">
-          <span class="input-group-btn">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="submit"><i class="fa fa-search"></i> Cari</button>
-            </span>
-          </span>
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css
+">
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h4><i class="fa fa-home"></i> Tabel Transaksi</h4>
+    <div class=row>
+      <div class="col-md-12 text-left" style="margin-bottom: 1.5rem">
+        <a href="{{Url('/transaksi/tambah')}}" class="btn btn-primary btn-sm"><i class="fa fa-plus-circle"></i> Tambah Transaksi</a>
       </div>
-      {!! Form::close() !!}
-  </div> -->
-  <div class="container">
-      <br><br>                                                                                        
-      <div class="table-responsive">          
-        <table class="table">
-          <thead>
+      <!-- /.col-md-12 -->
+      <div class="col-md-12">                                                                                      
+        <div class="table-responsive">          
+          <table id="myTable" class="table table-bordered table-condensed table-hover">
+            <thead>
               <tr>
-                <th>#</th>
-                <th>Id Transaksi</th>
-                <th>Waktu</th>
+                <th>Tanggal Dibuat</th>
+                <th>ID Transaksi</th>
+                <th>ID Quotation</th>
                 <th>Customer</th>
-                <th>Total Harga</th>
+                <th>Alamat</th>
+                <th>Detail</th>
               </tr>
-          </thead>
-          <tbody>
-          @foreach($transaksis as $key => $value)
-            <tr>
-              <td></td>
-              <td>{{ $value->id_tr }}</td>
-              <td>{{ $value->customer }}</td>
-              <td>{{ $value->alamt }}</td>
-              <td>{{ $value->created_at }}</td>
-              <td>
-              <a href="{{Url('/transaksi/detail')}}"
-                <button type="button" class="btn btn-default">Lihat</button></a>
-                <button type="button" class="btn btn-danger">Hapus</button>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-       </table>
+            </thead>
+            <tbody>
+            @foreach($transaksis as $key => $value)
+              <tr>
+                <td>{{ $value->created_at }}</td>
+                <td>{{ $value->id_tr }}</td>
+                <td>{{ $value->id_quo }}</td>
+                <td>{{ $value->customer }}</td>
+                <td>{{ $value->alamat }}</td>
+                <td><a href="{{Url('/transaksi/detail')}}"<button type="button" class="btn btn-default btn-sm"/><i class="fa fa-eye"></i> Lihat</button></a></td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
+    </div>
+    <!-- /.row -->
   </div>
 </div>
-<br>
-</div>
-<br>
+
+<script src="http://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js "></script>
+          
+<script>
+  $(document).ready(function(){
+    $('#myTable').DataTable();
+});
+</script>
 @stop
